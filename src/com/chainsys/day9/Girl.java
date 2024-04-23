a9362811557df58518aae2ab5fb5a056e3831b5c
Rakesh3555 <rakesh.arunagirisamy@chainsys.com>
2024-04-23 10:33:55 +0530

    Working on business logic

--- src/com/chainsys/day9/Girl.java
+++ src/com/chainsys/day9/Girl.java
@@ -1,12 +1,23 @@
 package com.chainsys.day9;
+import java.util.*;
 
 public class Girl implements Matrimony {
 
+	Scanner mg = new Scanner(System.in) ;
 
 	public void name() {
 		// TODO Auto-generated method stub
-		System.out.println("Gokula");
-		
+		String valName = "[a-zA-Z]{3,16}";
+		String name;
+		while(true) {
+			System.out.println("Enter your name : ");
+			name = mg.next();
+			if(name.matches(valName)) {
+				break;
+			}else {
+			System.out.println("Invalid entry");
+		}			
+	  }
 	}
 
 	
